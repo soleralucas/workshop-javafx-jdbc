bc0cf2c387bf43948a44077519f6d4a2c8f2c669
Lucas Solera <lucassolera2006@email.com>
2026-05-01 14:13:24 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,14 +10,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
+		
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -25,6 +28,12 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		launch(args);
